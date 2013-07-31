e926df2e53a7a9371b14cae79f9dde457fad77a3
jacobnlangley <jacob@jacobnlangley.com>
2013-07-30 21:32:55 -0400

    Add a user and install devise views

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+
+# Use devise for Authentication
 gem 'devise'
 
 # Use SCSS for stylesheets
--- db/migrate/20130728160859_devise_create_users.rb
+++ db/migrate/20130728160859_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :profile_name
-      
+
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -43,8 +43,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true
   end
 end
